8ceb635ac54cd3113017d9ae5d788eb22572dad4
Chris McKenna <chris.mediathread@gmail.com>
2016-09-15 00:01:20 -0400

    removed extra comments

--- fabfile.py
+++ fabfile.py
@@ -70,7 +70,3 @@ def zabbixINSTALL(PASSWORD):
 
 
 
-# wget http://repo.zabbix.com/zabbix/3.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_3.0-1+trusty_all.deb
-# dpkg -i zabbix-release_3.0-1+trusty_all.deb
-# apt-get update
-
